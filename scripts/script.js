66f7e4108f7cafd413ffa04f26b84a2128a90a30
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2019-11-16 16:23:24 -0600

    Add Search logic and refactor queryURL to generic

--- scripts/script.js
+++ scripts/script.js
@@ -4,28 +4,23 @@
 
 $(document).ready(function(){ 
 
-    function currentCondition() {
-        let queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=4724129&APPID=23dc5f87ddf3af5418217e5f3640466c&units=imperial";
+    function currentCondition(cityId) {
+        let queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=" + cityId + "&APPID=23dc5f87ddf3af5418217e5f3640466c&units=imperial";
         
         $.getJSON(queryURL, function(data){
             updateDOM(data);
         });
     }
 
-    currentCondition();
-
-    function UVdata() {
-        let uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=23dc5f87ddf3af5418217e5f3640466c&lat=30.5083&lon=-97.679"
+    function UVdata(lat, lon) {
+        let uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=23dc5f87ddf3af5418217e5f3640466c&lat=" + lat+ "&lon=" + lon;
 
         $.getJSON(uvURL, function(UV_data){
             updateUV(UV_data);
         });
     }
 
-    UVdata();
-
     function updateDOM(data) {
-
         let cityName = data.city.name;
         let iconCode = data.list[0].weather[0].icon;
         let iconURL = "http://openweathermap.org/img/w/" + iconCode + '.png';
@@ -42,26 +37,24 @@ $(document).ready(function(){
     }
 
     function updateUV(UV_data) {
-
         let showUV = UV_data.value;
-
         $('#UV').text("UV Index: " + showUV);
     }
 
+    //Function Calls for current conditions 
+
+    currentCondition(4724129);
+    UVdata(30.5083, -97.679);
 
     // 5-Day Forecast 
 
-    function fiveDayForecast() {
-        
-        let dayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?appid=23dc5f87ddf3af5418217e5f3640466c&id=4724129&count=10&units=imperial";
+    function fiveDayForecast(cityId) {
+        let dayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?appid=23dc5f87ddf3af5418217e5f3640466c&id=" + cityId + "&count=10&units=imperial";
 
         $.getJSON(dayForecastURL, function(fiveDaydata){
-
             updateForecast(fiveDaydata);
         });
     }
-
-    fiveDayForecast();
 
     function udpateData(fiveDayData, i, count) {
         let day = fiveDayData.list[i].dt_txt.substring(0,10);
@@ -77,25 +70,24 @@ $(document).ready(function(){
     }
 
     function updateForecast(fiveDaydata) {
-
         let size = fiveDaydata.list.length;
         let count = 1;
         let currDay = fiveDaydata.list[0].dt_txt.substring(0,10);
 
-        for (let i = 1 ; i < size; i++) {
+        for (let i = 0 ; i < (size) ; i++) {
             let day = fiveDaydata.list[i].dt_txt.substring(0,10);
-            //console.log(day);
             if (currDay != day) {
                 //console.log("Invoke a diff date "+ currDay, day);
                 udpateData(fiveDaydata, i, count);
                 currDay = day;
                 count++;
             } 
-            /*else {
-                console.log("current day and day equal "+ currDay, day);
-            }*/
         }
     }
+
+    // Function Calls for five day forecast 
+
+    fiveDayForecast(4724129);
 
     // Search for a city
 
@@ -103,24 +95,25 @@ $(document).ready(function(){
 
      $('#searchButton').on('click', function(event) {
       event.preventDefault();
-      let quest = $("#runSearch").val().trim();
-      let searchURL = "http://api.openweathermap.org/data/2.5/weather?APPID=23dc5f87ddf3af5418217e5f3640466c&units=imperial&q=" + quest;
-    
+      let quest = $("#runSearch").val().trim(); 
+      let searchURL = "http://api.openweathermap.org/data/2.5/forecast?APPID=23dc5f87ddf3af5418217e5f3640466c&units=imperial&q=" + quest;
+      
       $.getJSON(searchURL, function(searchData){
         console.log(searchData);          //For testing 
         console.log(quest);
-        console.log(searchURL);        
-        updateSearch(searchData);
-        });      
+        console.log(searchURL);
+
+        let cityId = searchData.city.id;
+        let latitude = searchData.city.coord.lat;
+        let longitude = searchData.city.coord.lon;
+
+        currentCondition(cityId);
+        UVdata(latitude, longitude);
+        fiveDayForecast(cityId);
+        }); 
      })
     }
 
     searchHistory();
 
-    function updateSearch(searchData) {
-        let cityId = searchData.id;
-        console.log(cityId);
-        updateDOM(searchData);
-        updateForecast(searchData);
-    }
 });
